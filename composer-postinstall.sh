c06cedbc49ee14769c579bc1399a36f2601e0401
Levani1111 <papashvililevani@gmail.com>
2023-05-29 16:57:11 +0100

    gitignore issue

--- composer-postinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Copying local wp-config.php";
-cp config/wordpress/wp-config.php.local ./wp-config.php
-
-echo "Copying plugins";
-#cp -r config/wordpress/plugins/* ./wp-content/plugins/
